0f2d2e39b4d54a94fc7cc0167f71fb3d570d3682
Ling Zhang <zhangling.ice@gmail.com>
2015-08-13 22:07:52 -0700

    add sql file and update ocp-graphite-test

--- ocp-graphite-test-0.3.rb
+++ ocp-graphite-test-0.3.rb
@@ -44,10 +44,13 @@ end
 
 
 ########## test for status check update
+
 require 'active_record'
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'ocp.db')
 class AppOfferings < ActiveRecord::Base
 end 
+class UrlChecks < ActiveRecord::Base
+end
 
 
 put '/status/api/v1/checks/id/update' do
@@ -58,23 +61,24 @@ put '/status/api/v1/checks/id/update' do
 	elsif !params.key? 'status'  
 		result = "{ 'error' : 'Please provide check's status.'}"
 	else
-
-		check = UrlChecks.find_by check_id: id
+		check_id = params['id']
+		check = UrlChecks.find_by check_id: check_id
 		app_sys_id = check.app_sys_id
 	    app = AppOfferings.find_by app_sys_id: app_sys_id
         app_name = app.app_name
 
 		metrics_data2 = { 
-			"intuit.#{app_name}.check-#{id}.response" => params['response'],
-			"intuit.#{app_name}.check-#{id}.status" => params['status']
+			"intuit.#{app_name}.check-#{check_id}.response" => params['response'],
+			"intuit.#{app_name}.check-#{check_id}.status" => params['status']
 		}  
 		gu.update_with_hash(metrics_data2)
 	end
 end
 
 # before the test, need to run following SQL in sqlite3 first
-# $ sqlite3 ocp.db
-#> CREATE TABLE "app_offerings" ("app_sys_id" TEXT NOT NULL, "app_name" TEXT, PRIMARY KEY("app_sys_id"));
-#> insert into app_offerings values ("1", "intuit.FPS");
+# $ sqlite3 ocp.db < ocp-test-sql.sql
+# then can test with curl or postman with put api
+# with id=c001, response=10, status=1
+
 
 
